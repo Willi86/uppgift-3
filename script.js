82cfab3cba7f5367721b3dc169f6385ecf8c55e1
Willi86 <williamlundqvist@yahoo.com>
2022-10-06 11:24:27 +0200

    I fixed issues

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/*function Hello2(){
+  let text = document.querySelector("body");
+  text.innerHTML = "Hello!";
+
+}
+setTimeout(Hello2, 3000);
+
+*/
+
+
+function Bye(){
+  let text = document.getElementById("h11");
+  text.innerHTML = "Goodbye!";
+
+
+}
+
+
+
+function Hello(){
+  let text = document.getElementById("2");
+  text.innerHTML = "Fixing this took me over 2 hours, Because i forgot to save HTML file changes!!";
+  text.style.color = "red";
+
+}
+
+
+
+
+console.log("This is a test");
+
+
+
+
